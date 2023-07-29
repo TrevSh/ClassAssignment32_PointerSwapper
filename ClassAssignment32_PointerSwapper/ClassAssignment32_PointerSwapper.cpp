16a32446957fc7044ee0b8b018185262df15d716
TrevSh <trevonshelby@outlook.com>
2023-07-28 19:03:40 -0500

    Add project files.

---
+++ ClassAssignment32_PointerSwapper/ClassAssignment32_PointerSwapper.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+
+void badSwap(int x, int y);
+void goodSwap(int* const pX, int* const pY);
+
+int main()
+{
+    int myScore = 150;
+    int yourScore = 1000;
+    std::cout << "Original values\n";
+    std::cout << "myScore: "<<myScore<<"\n";
+    std::cout << "yourScore: "<<yourScore<<"\n";
+
+    std::cout << "Calling badSwap()\n";
+    badSwap(myScore, yourScore);
+    std::cout << "myScore: " << myScore << "\n";
+    std::cout << "yourScore: " << yourScore << "\n";
+
+    std::cout << "Calling goodSwap()\n";
+    goodSwap(&myScore, &yourScore);
+    std::cout << "myScore: " << myScore << "\n";
+    std::cout << "yourScore: " << yourScore << "\n";
+
+    system("pause");
+}
+
+void badSwap(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void goodSwap(int* const pX, int* const pY) {
+    //store value pointed to by pX in temp
+    int temp = *pX;
+    //store value pointed to by pY in address pointed to by pX
+    *pX = *pY;
+    //store value originally pointed to by pX in address pointed to by pY
+    *pY = temp;
+}
